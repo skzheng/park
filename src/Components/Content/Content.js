c04a430e1016f8400ccba3b0ebe6828f0723ba12
Simon K Zheng <simonkzheng@gmail.com>
2018-04-19 02:30:40 -0400

    Content Complete

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -16,29 +16,89 @@ class Content extends React.Component {
     return (
       <div className="content">
         {/* <h1></h1> */}
-        <div className="user-host">
+        <div className="user-section">
         <div className="user" data-aos="fade-left" data-aos-duration="3000" data-aos-once="true">
           <div className="content-logo">
             <i className="fas fa-street-view"></i>
           </div>
-            <h1>Users</h1>
+            <h1 style={{fontWeight: '800'}}>USERS</h1>
             <Steps direction="vertical" current={-1}>
-              <Step title="EASY ACCESS" description="Park in convenient locations" />
-              <Step title="SAVE MONEY" description="Save thousands on tickets" />
-              <Step title="PLAN AHEAD" description="Reserve spaces in advance" />
+              <Step title="Choose" description="Pick an open location" />
+              <Step title="Confirm" description="Enter your make and model" />
+              <Step title="Submit" description="Pay and receive confirmation" />
             </Steps>
+            <div className="button">
+              <p className="button-text">GET STARTED</p>
+              <i class="fas fa-arrow-right"></i>
+            </div>
         </div>
         <div className="line" />
-        <div className="host" data-aos="fade-left" data-aos-duration="3000" data-aos-once="true">
+        <div className="user-benefits">
+          <div className="card" data-aos="fade-left" data-aos-duration="1500" data-aos-once="true" >
+            <i class="fas fa-check card-icon"></i>
+            <div className="card-content">
+              <h2 className="card-title">EASY ACCESS</h2>
+              <p className="card-text">Spend less time finding parking.</p>
+            </div>
+          </div>
+          <div className="card" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="500" data-aos-once="true">
+            <i className="far fa-money-bill-alt card-icon"></i>
+            <div className="card-content">
+              <h2 className="card-title">SAVE MONEY</h2>
+              <p className="card-text">Save hundreds on tickets and fines.</p>
+            </div>
+          </div>
+          <div className="card" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1000" data-aos-once="true">
+            <i class="far fa-calendar-alt card-icon"></i>
+            <div className="card-content">
+              <h2 className="card-title">PLAN AHEAD</h2>
+              <p className="card-text">Reserve spaces in advance.</p>
+            </div>
+          </div> 
+        </div>
+        </div>
+
+        {/* <div className="divider"/> */}
+
+        <div className="host-section">
+        <div className="user-benefits">
+          <div className="card" data-aos="fade-left" data-aos-duration="1500" data-aos-once="true" >
+            <i class="fas fa-fast-forward card-icon"></i>
+            <div className="card-content">
+              <h2 className="card-title">SIMPLE SET UP</h2>
+              <p className="card-text">Get started in less than 10 minutes</p>
+            </div>
+          </div>
+          <div className="card" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="500" data-aos-once="true">
+            <i class="fas fa-hand-holding-usd card-icon"></i>
+            <div className="card-content">
+              <h2 className="card-title">EARN MONEY</h2>
+              <p className="card-text">Generate passive income easily</p>
+            </div>
+          </div>
+          <div className="card" data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1000" data-aos-once="true">
+          <i class="fas fa-lock card-icon"></i>
+            <div className="card-content">
+              <h2 className="card-title">STAY SECURE</h2>
+              <p className="card-text">PWM parking sensors keep you safe</p>
+            </div>
+          </div> 
+        </div>
+        <div className="line" />
+        <div className="host"  data-aos="fade-left" data-aos-duration="3000" data-aos-once="true">
           <div className="content-logo">
             <i className="far fa-building"></i>
           </div>
-            <h1>Hosts</h1>
+            <h1 style={{fontWeight: '800'}}>HOSTS</h1>
             <Steps direction="vertical" current={-1}>
-                <Step title="Simple set up" description="Host a spot quickly" />
-                <Step title="Earn Money" description="Generate passive income" />
-                <Step title="Waiting" description="This is a description." />
+                <Step title="Register" description="Create an account" />
+                <Step title="Host" description="Add open parking locations" />
+                <Step title="Accept" description="Confirm parking requests" />
               </Steps>
+            <div className="button">
+              <p className="button-text">GET STARTED</p>
+              <i class="fas fa-arrow-right"></i>
+            </div>
         </div>
         </div>
       </div>
